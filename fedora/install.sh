957d97e702b997d0741d8e733f96f2162698779d
Jean-Alexandre Barszcz <jalex_b@hotmail.com>
2016-04-22 21:04:12 -0400

    changed commands to make unattended install

--- fedora/install.sh
+++ fedora/install.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 
 # Dev environment
-dnf install emacs git gitk screen autojump wget eclipse subversion mc
-dnf install htop stress procinfo tree
-dnf install macchanger nmap
+dnf install -y emacs git gitk screen autojump wget eclipse subversion mc
+dnf install -y htop stress procinfo tree
+dnf install -y macchanger nmap
 
 # Dev tools
 dnf groupinstall "C Development Tools and Libraries"
-dnf install haskell-platform emacs-haskell-mode hlint
-#dnf install stack ... ?
-dnf install python-pip  sympy python-ipython scipy python3-scipy
-dnf install lttng-tools
-dnf install discount # markdown
+dnf install -y haskell-platform emacs-haskell-mode hlint
+#dnf install -y stack ... ?
+dnf install -y python-pip  sympy python-ipython scipy python3-scipy
+dnf install -y lttng-tools
+dnf install -y discount # markdown
 
 # Other repos
-dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
 # Window manager
 ## Xmonad
-dnf install xcompmgr xmonad xmobar scrot xscreensaver-base gnome-screensaver
-dnf install rxvt-unicode-256color xbacklight wireless-tools mpc ncmpcpp mpd
-dnf install NetworkManager-tui feh
+dnf install -y xcompmgr xmonad xmobar scrot xscreensaver-base gnome-screensaver
+dnf install -y rxvt-unicode-256color xbacklight wireless-tools mpc ncmpcpp mpd
+dnf install -y NetworkManager-tui feh
 
 ## Gnome
-dnf install gnome-tweak-tool
+dnf install -y gnome-tweak-tool
 
 # Other
-dnf install solfege lynx liveusb-creator units unrar vlc
-dnf install gimp inkscape dia darktable fbida xournal
-dnf install aspell-en aspell-fr hunspell-fr verbiste
-dnf install texlive djvulibre graphviz texlive-babel-french texlive-babel-french-doc libxslt texlive-lettre
-dnf install figlet cowsay fortune-mod
-dnf install lame audacity
-dnf install opencv-devel
-# dnf install google-chrome-stable ## Needs the google repo
+dnf install -y solfege lynx liveusb-creator units unrar vlc
+dnf install -y gimp inkscape dia darktable fbida xournal
+dnf install -y aspell-en aspell-fr hunspell-fr verbiste
+dnf install -y texlive djvulibre graphviz texlive-babel-french texlive-babel-french-doc libxslt texlive-lettre
+dnf install -y figlet cowsay fortune-mod
+dnf install -y lame audacity
+dnf install -y opencv-devel
+# dnf install -y google-chrome-stable ## Needs the google repo
